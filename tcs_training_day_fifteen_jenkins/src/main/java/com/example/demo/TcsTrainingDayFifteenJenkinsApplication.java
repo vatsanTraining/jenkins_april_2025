f99a8d4a82a2feb803951990e393a1eb23fd128a
srivatsan <vatsan_k@hotmail.com>
2025-05-06 10:31:44 +0530

    Initial commit

---
+++ tcs_training_day_fifteen_jenkins/src/main/java/com/example/demo/TcsTrainingDayFifteenJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcsTrainingDayFifteenJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcsTrainingDayFifteenJenkinsApplication.class, args);
+	}
+
+}
